c5076577f36a381cbea55321a2b25ab1203f92d1
Giulia Passoni <giulia@colour-profiling.com>
2023-03-06 09:21:01 +0000

    feat: introduced sass variables and general styling, following figma mock-up

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link} from "react-router-dom";
+import style from './../style/navbar.module.scss'
 
 import NavbarLink from './../components/NavbarLink'
 
@@ -7,8 +8,8 @@ export default function Navbar(){
 
     return (
         <nav>
-            <Link to='/'>
-                <h1>Hi</h1>
+            <Link to='/' className={style.logo}>
+                <h1>LOGO HERE</h1>
             </Link>
             <ul>
                 <li>
@@ -17,8 +18,18 @@ export default function Navbar(){
                     </Link>
                 </li>
                 <li>
+                    <Link to='/case-study'>
+                        <NavbarLink linkName={'Case Study'} />
+                    </Link>
+                </li>
+                <li>
                     <Link to='/shop'>
                         <NavbarLink linkName={'Shop'} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/contact'>
+                        <NavbarLink linkName={'Contact'} />
                     </Link>
                 </li>
             </ul>
